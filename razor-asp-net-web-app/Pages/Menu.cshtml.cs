445d44b6b3fe0ae35f22b3829e47ed9b4ce91816
Josias <josias.qr@hotmail.com>
2023-10-03 18:38:35 -0500

    first commit

---
+++ razor-asp-net-web-app/Pages/Menu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor_asp_net_web_app.Pages
+{
+    public class MenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
